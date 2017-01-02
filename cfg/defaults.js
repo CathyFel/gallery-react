6113cd0099857f0bd2ca9eb3438411c51958d56b
Cathy Ren <507150751@qq.com>
2017-01-02 13:14:58 +0800

    Update defaults.js

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -62,7 +62,7 @@ function getDefaultModules() {
 
 module.exports = {
   srcPath: srcPath,
-  publicPath: 'gallery-react/assets/',
+  publicPath: './assets/',
   port: dfltPort,
   getDefaultModules: getDefaultModules
 };
